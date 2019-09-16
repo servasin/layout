10a90a0f8a48e0792f417b3fb8d5549684a166f7
Sergey Vasin <48726399+servasin@users.noreply.github.com>
2019-09-16 17:34:03 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementsByClassName("elements")[0].classList.add("elements--active");
+
+});
+
+// $(document).ready(function() {
+//   $('.elements').addClass('elements--active');
+// });
